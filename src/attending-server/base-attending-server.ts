8b6549dcd58ec5de6289e13bf535c6e61606cbbb
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2022-08-25 02:10:37 -0700

    feat: role creation

--- src/attending-server/base-attending-server.ts
+++ src/attending-server/base-attending-server.ts
@@ -5,6 +5,8 @@ import {
     TextChannel,
     User,
     GuildMember,
+    ColorResolvable,
+    RoleData,
 } from "discord.js";
 import { HelpQueue } from "../queue";
 import { UserError } from "../user_action_error";
@@ -12,6 +14,7 @@ import { MemberStateV2 } from "../models/member-states";
 import { EmbedColor, SimpleEmbed } from "../embed_helper";
 import { Firestore } from "firebase-admin/firestore";
 import { CommandChConfig } from "./command-ch-constants";
+import { hierarchyRoleConfigs } from "../models/access-level";
 
 // Wrapper for TextChannel
 // Guarantees that a queue_name exists
@@ -19,6 +22,12 @@ type QueueChannel = {
     channelObject: TextChannel;
     queueName: string;
 };
+
+interface BOBServer {
+    getQueueChannels: (use_cache: boolean) => Promise<QueueChannel[]>;
+    initAllQueues: () => Promise<void>;
+    updateCommandHelpChannels: () => Promise<void>;
+}
 
 class AttendingServerV2 {
     private queues: HelpQueue[] = [];
@@ -28,11 +37,11 @@ class AttendingServerV2 {
         private readonly guild: Guild,
         private readonly firebaseDB: Firestore,
         private memberStates = new Map<GuildMember, MemberStateV2>()
-    ) { }
+    ) {}
 
     /**
-     * **Asynchronously creates a YABOB instance for 1 server**
-     *
+     * Asynchronously creates a YABOB instance for 1 server
+     * ----
      * @param user discord client user
      * @param server the server for YABOB to join
      * @param firebaseDB firebase database object
@@ -49,7 +58,9 @@ class AttendingServerV2 {
             await owner.send(
                 SimpleEmbed(
                     `Sorry. I need full administrator permission to join and manage "${server.name}"`,
-                    EmbedColor.Error));
+                    EmbedColor.Error
+                )
+            );
             await server.leave();
             throw new UserError("YABOB doesn't have admin permission.");
         }
@@ -57,13 +68,17 @@ class AttendingServerV2 {
         console.log(`Creating new YABOB for server: ${server.name}`);
         const me = new AttendingServerV2(user, server, firebaseDB);
 
-        await me.updateCommandHelpChannels();
+        // await me.createHierarchyRoles();
+        await me.createClassRoles();
+
+        // await me.updateCommandHelpChannels();
 
         return me;
     }
 
     /**
      * Gets all the queue channels on the server
+     * ----
      * if nothing is found, returns empty array
      * @param use_cache whether to read from existing cache, defaults to true
      * - unless queues change often, prefer cache for fast response
@@ -80,8 +95,7 @@ class AttendingServerV2 {
             .map(category => [
                 category.children.find(
                     child =>
-                        child.name === "queue" &&
-                        child.type === "GUILD_TEXT"
+                        child.name === "queue" && child.type === "GUILD_TEXT"
                 ),
                 category.name,
             ])
@@ -133,8 +147,6 @@ class AttendingServerV2 {
             .map(ch => ch as CategoryChannel);
 
         // If no help category is found, initialize
-        // ! initialization only in this if block
-        // messages are handled separately
         if (existingHelpCategory.length === 0) {
             console.log("\x1b[33mFound no help channels. Creating new ones.");
 
@@ -145,56 +157,117 @@ class AttendingServerV2 {
             existingHelpCategory.push(helpCategory);
 
             for (const role of Object.values(CommandChConfig)) {
-                const commandCh = await helpCategory.createChannel(role.channelName);
+                const commandCh = await helpCategory.createChannel(
+                    role.channelName
+                );
 
                 // ? doesn't block server owner
                 await commandCh.permissionOverwrites.create(
                     this.guild.roles.everyone,
-                    { SEND_MESSAGES: false });
-                await commandCh.permissionOverwrites.create(
-                    this.user,
-                    { SEND_MESSAGES: true });
+                    { SEND_MESSAGES: false }
+                );
+                await commandCh.permissionOverwrites.create(this.user, {
+                    SEND_MESSAGES: true,
+                });
 
                 // ** Change the config object and add more function calls here if necessary
             }
         } else {
-            console.log('\x1b[33mFound existing help channel, updating command help file\x1b[0m');
+            console.log(
+                "\x1b[33mFound existing help channel, updating command help file\x1b[0m"
+            );
         }
 
         await this.sendCommandHelpMessages(existingHelpCategory);
     }
 
-    // async createRoles(): Promise<void> {
+    /**
+     * Creates all the command access hierarchy roles
+     * ----
+     */
+    async createHierarchyRoles(): Promise<void> {
+        const existingRoles = (await this.guild.roles.fetch()).map(
+            role => role.name
+        );
+
+        // create all the missing roles to bump up YABOB's role
+        await Promise.all(
+            hierarchyRoleConfigs
+                .filter(roleConfig => !existingRoles.includes(roleConfig.name))
+                .map(
+                    async roleConfig =>
+                        await this.guild.roles.create(roleConfig)
+                )
+        );
+
+        console.log("Initially create roles:");
+        console.log(this.guild.roles.cache.map(r => [r.name, r.position]));
+    }
+
+    /**
+     * Creates roles for all the available queues if not already created
+     * ----
+     */
+    async createClassRoles(): Promise<void> {
+        const existingRoles = this.guild.roles.cache.map(role => role.name);
+        const queueNames = (await this.getQueueChannels()).map(
+            ch => ch.queueName
+        );
+        await Promise.all(
+            queueNames
+                .filter(queue => !existingRoles.includes(queue))
+                .map(
+                    async roleToCreate =>
+                        await this.guild.roles.create({
+                            name: roleToCreate,
+                            position: 1,
+                        })
+                )
+        );
+    }
+
+    // async hieararchyRolesAreCorrect(): Promise<boolean> {
 
     // }
 
-    private async sendCommandHelpMessages(helpCategories: CategoryChannel[]): Promise<void> {
-        const allHelpChannels = helpCategories
-            .flatMap(cat => [...cat.children.values()]
-                .filter(ch => ch.type === 'GUILD_TEXT') as TextChannel[]);
+    private async sendCommandHelpMessages(
+        helpCategories: CategoryChannel[]
+    ): Promise<void> {
+        const allHelpChannels = helpCategories.flatMap(
+            cat =>
+                [...cat.children.values()].filter(
+                    ch => ch.type === "GUILD_TEXT"
+                ) as TextChannel[]
+        );
 
         if (helpCategories.length === 0 || allHelpChannels.length === 0) {
-            console.warn('\x1b[31mNo help categories found.\x1b[0m');
-            console.log('Did you mean to call \x1b[32mupdateCommandHelpChannels()\x1b[0m?');
+            console.warn("\x1b[31mNo help categories found.\x1b[0m");
+            console.log(
+                "Did you mean to call \x1b[32mupdateCommandHelpChannels()\x1b[0m?"
+            );
             return;
         }
 
         // delete all existing messages
-        await Promise.all(allHelpChannels
-            .map(async (ch) => {
-                await ch.messages.fetch()
+        await Promise.all(
+            allHelpChannels.map(async ch => {
+                await ch.messages
+                    .fetch()
                     .then(messages => messages.map(msg => msg.delete()));
-            }));
+            })
+        );
 
         // now send new ones
-        await Promise.all(allHelpChannels
-            .map(async (ch) => {
-                const file = Object.values(CommandChConfig)
-                    .find(val => val.channelName === ch.name)?.file;
+        await Promise.all(
+            allHelpChannels.map(async ch => {
+                const file = Object.values(CommandChConfig).find(
+                    val => val.channelName === ch.name
+                )?.file;
                 if (file) {
                     await ch.send(SimpleEmbed(file));
                 }
-            }));
+            })
+        );
     }
 }
 
--- src/models/access-level.ts
+++ src/models/access-level.ts
@@ -1,5 +1,34 @@
+import { ColorResolvable } from "discord.js";
+
 export enum CommandAccessLevel {
     ANYONE,
     STAFF,
     ADMIN,
 }
+
+/**
+ * Order the config objects in the order of hight -> low hierearchy
+ * If the bot starts with an empty server,
+ * there won't be enough indexes to raise the bot's position high enough
+*/
+
+export const hierarchyRoleConfigs = [
+    {
+        name: "Admin",
+        color: "DARK_VIVID_PINK" as ColorResolvable,
+        hoist: true,
+        position: 1
+    },
+    {
+        name: "Staff",
+        color: "RED" as ColorResolvable,
+        hoist: true,
+        position: 1
+    },
+    {
+        name: "Student",
+        color: "GREEN" as ColorResolvable, // casting is safe here
+        hoist: true,
+        position: 1
+    }
+];
