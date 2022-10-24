a8bdc9064699addd4541acdc6c16c70a943ec4b9
Kaoushik Murugan <kaoushikmurugan@gmail.com>
2022-10-24 11:39:26 -0700

    Cleanup: minor changes to presenceList
    added comments and added a couple more presences

--- src/utils/discord-presence.ts
+++ src/utils/discord-presence.ts
@@ -11,21 +11,28 @@ type BotPresence = {
 };
 
 const presenceList: BotPresence[] = [
+    // Bob The Builder
     { name: 'Bob The Builder', type: ActivityType.Watching },
     { name: 'Can We Fix It?', type: ActivityType.Listening },
+    // Memes
     { name: 'Never Gonna Give You Up', type: ActivityType.Listening },
     { name: 'Shrek', type: ActivityType.Watching },
     { name: 'Crave Rave', type: ActivityType.Listening },
     { name: 'All Star', type: ActivityType.Listening },
     { name: 'Dragostea Din Tei', type: ActivityType.Listening },
+    { name: 'HEYYEYAAEYAAAEYAEYAA', type: ActivityType.Listening },
+    // Number of servers
     { name: `${client.guilds.cache.size} servers`, type: ActivityType.Watching },
+    // CS real
     { name: 'your bugs disappear', type: ActivityType.Watching },
+    { name: 'you squash your bugs', type: ActivityType.Watching },
     { name: 'you code', type: ActivityType.Watching },
     { name: 'Lectures', type: ActivityType.Watching },
     { name: 'Midterm Review', type: ActivityType.Watching },
     { name: 'Finals Review', type: ActivityType.Watching },
     { name: 'Hello World Tutorial', type: ActivityType.Watching },
-    { name: 'Coding Tutorials', type: ActivityType.Watching }
+    { name: 'Coding Tutorials', type: ActivityType.Watching },
+    
 ];
 
 let previousPresence: BotPresence | undefined = undefined;
