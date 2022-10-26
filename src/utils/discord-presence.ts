e4eeb37dd89e27254659e2abfd0063691718db3a
Kaoushik Murugan <kaoushikmurugan@gmail.com>
2022-10-26 01:11:49 -0700

    Fix: formatting in presencelist
    bonus fix: spelling error: it was `gbd` instead of `gdb`

--- src/utils/discord-presence.ts
+++ src/utils/discord-presence.ts
@@ -13,72 +13,66 @@ type StaticBotPresence = {
 
 const staticPresenceList: StaticBotPresence[] = [
     // Bob The Builder
-    { type: ActivityType.Watching, name: 'Bob The Builder' },
+    { type: ActivityType.Watching,  name: 'Bob The Builder' },
     { type: ActivityType.Listening, name: 'Can We Fix It?' },
     // Memes
     { type: ActivityType.Listening, name: 'Never Gonna Give You Up' },
-    { type: ActivityType.Watching, name: 'Shrek' },
+    { type: ActivityType.Watching,  name: 'Shrek' },
     { type: ActivityType.Listening, name: 'Crave Rave' },
     { type: ActivityType.Listening, name: 'All Star' },
     { type: ActivityType.Listening, name: 'Dragostea Din Tei' },
     { type: ActivityType.Listening, name: 'HEYYEYAAEYAAAEYAEYAA' },
-    { type: ActivityType.Playing, name: 'Did you know that yabob is a real place?' },
+    { type: ActivityType.Playing,   name: 'Did you know that yabob is a real place?' },
     // CS real
-    { type: ActivityType.Watching, name: 'you squash your bugs' },
-    { type: ActivityType.Watching, name: 'you code' },
-    { type: ActivityType.Watching, name: 'Lectures' },
-    { type: ActivityType.Watching, name: 'Midterm Review' },
-    { type: ActivityType.Watching, name: 'Finals Review' },
-    { type: ActivityType.Watching, name: 'Hello World Tutorial' },
-    { type: ActivityType.Watching, name: 'Coding Tutorials' },
-    { type: ActivityType.Playing, name: 'Manifesting an A' },
-    { type: ActivityType.Playing, name: 'gbd' },
-    { type: ActivityType.Playing, name: 'leetcode' },
-    { type: ActivityType.Playing, name: 'hackerrank' },
-    { type: ActivityType.Playing, name: 'codeforces' },
+    { type: ActivityType.Watching,  name: 'you squash your bugs' },
+    { type: ActivityType.Watching,  name: 'you code' },
+    { type: ActivityType.Watching,  name: 'Lectures' },
+    { type: ActivityType.Watching,  name: 'Midterm Review' },
+    { type: ActivityType.Watching,  name: 'Finals Review' },
+    { type: ActivityType.Watching,  name: 'Hello World Tutorial' },
+    { type: ActivityType.Watching,  name: 'Coding Tutorials' },
+    { type: ActivityType.Playing,   name: 'Manifesting an A' },
+    { type: ActivityType.Playing,   name: 'gdb' },
+    { type: ActivityType.Playing,   name: 'leetcode' },
+    { type: ActivityType.Playing,   name: 'hackerrank' },
+    { type: ActivityType.Playing,   name: 'codeforces' },
     // CS Memes
-    { type: ActivityType.Playing, name: 'Faster than Internet Explorer' },
-    { type: ActivityType.Playing, name: 'Changing the TV input' },
-    { type: ActivityType.Playing, name: 'Never sudo rm -rf /' },
-    { type: ActivityType.Playing, name: 'crtl + C, ctrl + V' },
-    { type: ActivityType.Playing, name: 'cmd + C, cmd + V' },
-    { type: ActivityType.Watching, name: 'vim or emacs?' },
-    { type: ActivityType.Playing, name: 'git commit -m "fix"' },
-    { type: ActivityType.Playing, name: '#define true (rand() > 10)' },
-    { type: ActivityType.Playing, name: 'did you mean "XOR" or "OR"?' },
-    { type: ActivityType.Playing, name: "Don't use whitespace in C challenge" },
-    { type: ActivityType.Playing, name: '5 ! = 120' },
-    { type: ActivityType.Playing, name: '1 + 1 = 11' },
-    { type: ActivityType.Playing, name: "It's not a bug, it's a feature" },
-    { type: ActivityType.Playing, name: 'You probably need a <br>' },
-    { type: ActivityType.Playing, name: `exec(s:='print("exec(s:=%r)"%s)')` }, // Quine
+    { type: ActivityType.Playing,   name: 'Faster than Internet Explorer' },
+    { type: ActivityType.Playing,   name: 'Changing the TV input' },
+    { type: ActivityType.Playing,   name: 'Never sudo rm -rf /' },
+    { type: ActivityType.Playing,   name: 'crtl + C, ctrl + V' },
+    { type: ActivityType.Playing,   name: 'cmd + C, cmd + V' },
+    { type: ActivityType.Watching,  name: 'vim or emacs?' },
+    { type: ActivityType.Playing,   name: 'git commit -m "fix"' },
+    { type: ActivityType.Playing,   name: '#define true (rand() > 10)' },
+    { type: ActivityType.Playing,   name: 'did you mean "XOR" or "OR"?' },
+    { type: ActivityType.Playing,   name: "Don't use whitespace in C challenge" },
+    { type: ActivityType.Playing,   name: '5 ! = 120' },
+    { type: ActivityType.Playing,   name: '1 + 1 = 11' },
+    { type: ActivityType.Playing,   name: "It's not a bug, it's a feature" },
+    { type: ActivityType.Playing,   name: 'You probably need a <br>' },
+    { type: ActivityType.Playing,   name: `exec(s:='print("exec(s:=%r)"%s)')` }, // Quine
     // Hello World in different languages
-    { type: ActivityType.Playing, name: 'printf("Hello World");' }, // c
-    { type: ActivityType.Playing, name: 'System.out.println("Hello World");' }, // java
-    { type: ActivityType.Playing, name: 'print("Hello World")' }, // python
-    { type: ActivityType.Playing, name: 'puts "Hello World"' }, // ruby
-    { type: ActivityType.Playing, name: 'cout << "Hello World";' }, // c++
-    { type: ActivityType.Playing, name: 'console.log("Hello World");' }, // javascript
-    { type: ActivityType.Playing, name: 'fmt.Println("Hello World");' }, // go
-    { type: ActivityType.Playing, name: 'println!("Hello World"); // rust' }, // rust
-    { type: ActivityType.Playing, name: 'System.Console.WriteLine("Hello World");' }, // C#
-    { type: ActivityType.Playing, name: 'println("Hello World")' }, // swift
-    { type: ActivityType.Playing, name: "writeln ('Hello, world.');" }, // pascal
-    { type: ActivityType.Playing, name: 'echo "Hello World";' }, // php
-    { type: ActivityType.Playing, name: '(print "Hello World")' }, // lisp
-    { type: ActivityType.Playing, name: 'print *, "Hello World"' }, // fortran
-    { type: ActivityType.Playing, name: '10 PRINT "Hello World!"' }, // BASIC
-    { type: ActivityType.Playing, name: 'DISPLAY "Hello World!"' }, // cobol
-    {
-        type: ActivityType.Playing,
-        name: '++++++++++[>+++++++>++++++++++>+++<<<-]>++.>+.+++++++\
-..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.'
-    }, // brainf**k
-    {
-        type: ActivityType.Playing,
-        name: '(=<`#9]~6ZY327Uv4-QsqpMn&+Ij"\'E%e{Ab~w=_:]Kw%o44Uqp0/Q?xNvL:\
-`H%c#DD2^WV>gY;dts76qKJImZkj'
-    } // malbolge
+    { type: ActivityType.Playing,   name: 'printf("Hello World");' }, // c
+    { type: ActivityType.Playing,   name: 'System.out.println("Hello World");' }, // java
+    { type: ActivityType.Playing,   name: 'print("Hello World")' }, // python
+    { type: ActivityType.Playing,   name: 'puts "Hello World"' }, // ruby
+    { type: ActivityType.Playing,   name: 'cout << "Hello World";' }, // c++
+    { type: ActivityType.Playing,   name: 'console.log("Hello World");' }, // javascript
+    { type: ActivityType.Playing,   name: 'fmt.Println("Hello World");' }, // go
+    { type: ActivityType.Playing,   name: 'println!("Hello World"); // rust' }, // rust
+    { type: ActivityType.Playing,   name: 'System.Console.WriteLine("Hello World");' }, // C#
+    { type: ActivityType.Playing,   name: 'println("Hello World")' }, // swift
+    { type: ActivityType.Playing,   name: "writeln ('Hello, world.');" }, // pascal
+    { type: ActivityType.Playing,   name: 'echo "Hello World";' }, // php
+    { type: ActivityType.Playing,   name: '(print "Hello World")' }, // lisp
+    { type: ActivityType.Playing,   name: 'print *, "Hello World"' }, // fortran
+    { type: ActivityType.Playing,   name: '10 PRINT "Hello World!"' }, // BASIC
+    { type: ActivityType.Playing,   name: 'DISPLAY "Hello World!"' }, // cobol
+    { type: ActivityType.Playing,   name: '++++++++++[>+++++++>++++++++++>+++<<<-]>++.>+.+++++++\
+..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.' }, // brainf**k
+    { type: ActivityType.Playing,   name: '(=<`#9]~6ZY327Uv4-QsqpMn&+Ij"\'E%e{Ab~w=_:]\
+Kw%o44Uqp0/Q?xNvL:`H%c#DD2^WV>gY;dts76qKJImZkj' } // malbolge
 ];
 
 /**
@@ -89,10 +83,7 @@ const staticPresenceList: StaticBotPresence[] = [
 const dynamicPresenceList: Array<() => StaticBotPresence> = [
     // Number of servers, numGuilds: number
     () => {
-        return {
-            type: ActivityType.Watching,
-            name: `${client.guilds.cache.size} servers`
-        };
+        return { type: ActivityType.Watching, name: `${client.guilds.cache.size} servers` };
     }
 ];
 
