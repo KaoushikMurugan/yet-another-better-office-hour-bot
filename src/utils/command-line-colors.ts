f67c76022a6cc20832e0db64867b786eff09eaa0
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2023-11-13 20:37:11 -0800

    style: reduce clutter

--- src/utils/command-line-colors.ts
+++ src/utils/command-line-colors.ts
@@ -2,14 +2,7 @@
 
 import { Optional } from './type-aliases.js';
 
-/* eslint-disable @typescript-eslint/no-unused-vars */
-export const ResetColor = '\x1b[0m';
-export const Bright = '\x1b[1m';
-export const Dim = '\x1b[2m';
-export const Underscore = '\x1b[4m';
-export const Blink = '\x1b[5m';
-export const Reverse = '\x1b[7m';
-export const Hidden = '\x1b[8m';
+const ResetColor = '\x1b[0m';
 
 const FgBlack = '\x1b[30m';
 const FgRed = '\x1b[31m';
@@ -34,24 +27,36 @@ const BgWhite = '\x1b[47m';
  * @param str: string to color
  * @param fgOrBg: color the foreground or the background
  */
-export function red(str: Optional<string>, fgOrBg: 'Fg' | 'Bg' = 'Fg'): string {
+function red(str: Optional<string>, fgOrBg: 'Fg' | 'Bg' = 'Fg'): string {
     return `${fgOrBg === 'Fg' ? FgRed : BgRed}${str}${ResetColor}`;
 }
-export function green(str: Optional<string>, fgOrBg: 'Fg' | 'Bg' = 'Fg'): string {
+
+function green(str: Optional<string>, fgOrBg: 'Fg' | 'Bg' = 'Fg'): string {
     return `${fgOrBg === 'Fg' ? FgGreen : BgGreen}${str}${ResetColor}`;
 }
-export function yellow(str: Optional<string>, fgOrBg: 'Fg' | 'Bg' = 'Fg'): string {
+
+function yellow(str: Optional<string>, fgOrBg: 'Fg' | 'Bg' = 'Fg'): string {
     return `${fgOrBg === 'Fg' ? FgYellow : BgYellow}${str}${ResetColor}`;
 }
-export function blue(str: Optional<string>, fgOrBg: 'Fg' | 'Bg' = 'Fg'): string {
+
+function blue(str: Optional<string>, fgOrBg: 'Fg' | 'Bg' = 'Fg'): string {
     return `${fgOrBg === 'Fg' ? FgBlue : BgBlue}${str}${ResetColor}`;
 }
-export function magenta(str: Optional<string>, fgOrBg: 'Fg' | 'Bg' = 'Fg'): string {
+
+function magenta(str: Optional<string>, fgOrBg: 'Fg' | 'Bg' = 'Fg'): string {
     return `${fgOrBg === 'Fg' ? FgMagenta : BgMagenta}${str}${ResetColor}`;
 }
-export function cyan(str: Optional<string>, fgOrBg: 'Fg' | 'Bg' = 'Fg'): string {
+
+function cyan(str: Optional<string>, fgOrBg: 'Fg' | 'Bg' = 'Fg'): string {
     return `${fgOrBg === 'Fg' ? FgCyan : BgCyan}${str}${ResetColor}`;
 }
-export function black(str: Optional<string>, fgOrBg: 'Fg' | 'Bg' = 'Fg'): string {
+
+function black(str: Optional<string>, fgOrBg: 'Fg' | 'Bg' = 'Fg'): string {
     return `${fgOrBg === 'Fg' ? FgBlack : BgBlack}${str}${ResetColor}`;
 }
+
+function white(str: Optional<string>, fgOrBg: 'Fg' | 'Bg' = 'Fg'): string {
+    return `${fgOrBg === 'Fg' ? FgWhite : BgWhite}${str}${ResetColor}`;
+}
+
+export { red, green, yellow, blue, magenta, cyan, black, white };
