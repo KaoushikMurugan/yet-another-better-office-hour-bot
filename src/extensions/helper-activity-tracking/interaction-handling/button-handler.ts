647f6983fa3135f8c82b10208ada031ee7cde520
KMMineCube <63953483+KaoushikMurugan@users.noreply.github.com>
2024-06-23 20:20:23 -0700

    FEAT: REMOVED GOOGLE SHEETS
    LETS GOOO, NO MORE STUPID GOOGLE SHEET API ERRORS!!!! 🥳🥳
    
    ALL BACKUPS ON FIREBASE 🎉🎉

---
+++ src/extensions/helper-activity-tracking/interaction-handling/button-handler.ts
@@ -0,0 +1,47 @@
+import { ButtonInteraction } from 'discord.js';
+import { AttendingServer } from '../../../attending-server/base-attending-server.js';
+import { ButtonHandlerProps } from '../../../interaction-handling/handler-interface.js';
+import { ActivityTrackingButtonNames } from '../constants/interaction-names.js';
+import { ActivityTrackingSettingsConfigMenu } from '../constants/settings-menu.js';
+import { ActivityTrackingSuccessMessages } from '../constants/success-messages.js';
+
+const googleSheetButtonMap: ButtonHandlerProps = {
+    guildMethodMap: {
+        queue: {},
+        other: {
+            [ActivityTrackingButtonNames.UpdateTrackingStatus]: updateTrackingStatus,
+        }
+    },
+    dmMethodMap: {},
+    skipProgressMessageButtons: new Set([
+        ActivityTrackingButtonNames.UpdateTrackingStatus
+    ])
+};
+
+/**
+ * Updates sheet tracking setting
+ * @param interaction
+ */
+async function updateTrackingStatus(
+    interaction: ButtonInteraction<'cached'>
+): Promise<void> {
+    const server = AttendingServer.get(interaction.guildId);
+    const newTrackingStatus = !server.trackingEnabled;
+
+    server.setTrackingEnabled(newTrackingStatus);
+    server.sendLogMessage(
+        ActivityTrackingSuccessMessages.updatedSheetTracking(newTrackingStatus)
+    );
+
+    await interaction.update(
+        ActivityTrackingSettingsConfigMenu(
+            server,
+            false,
+            `Successfully ${
+                newTrackingStatus ? 'enabled' : 'disabled'
+            } google sheet tracking.`
+        )
+    );
+}
+
+export { googleSheetButtonMap };
--- src/interaction-handling/interaction-entry-point.ts
+++ src/interaction-handling/interaction-entry-point.ts
@@ -41,7 +41,6 @@ import { baseYabobModalMap } from './modal-handler.js';
 import { InteractionExtension } from '../extensions/extension-interface.js';
 import { SessionCalendarInteractionExtension } from '../extensions/session-calendar/calendar-interaction-extension.js';
 import { environment } from '../environment/environment-manager.js';
-import { GoogleSheetInteractionExtension } from '../extensions/google-sheet-logging/google-sheet-interaction-extension.js';
 import { AttendingServer } from '../attending-server/base-attending-server.js';
 import { HelperActivityTrackingInteractionExtension } from '../extensions/helper-activity-tracking/interaction-extension.js';
 
@@ -55,7 +54,6 @@ const interactionExtensions: ReadonlyArray<InteractionExtension> =
         : [
               // Do not use async creation methods here for now bc it conflicts with client login
               new SessionCalendarInteractionExtension(),
-              new GoogleSheetInteractionExtension(),
               new HelperActivityTrackingInteractionExtension()
           ];
 
